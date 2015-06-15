5bdf1c3bbe40c97845eaadf0ebc3604ad86b6425
Karl Podger <karl.podger@primeordinal.com>
2015-06-15 18:03:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Vehicular Grand Larceny
+
+
+This repository contains the source code for [Vehicular Grand Larceny](http://www.vehicular-grand-larceny.com/)
---
+++ configuration/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+cd $dir/..
+
+touch www/.htaccess
+#chown $1:httpd .htaccess
+#chown -R $1:httpd *
+find www -type d -exec chmod 755 {} \;
+find www -type f -exec chmod 644 {} \;
+chmod g+w www/.htaccess
+
+mkdir -p www/wp-content/themes/vehicular-grand-larceny/styleguide/templates && cp -r node_modules/gulp-dss/templates www/wp-content/themes/vehicular-grand-larceny/styleguide
