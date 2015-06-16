58e4e6ff69c855bb29b9df865ef32e0ac75b287f
Karl Podger <karl.podger@primeordinal.com>
2015-06-16 18:49:21 +0100

    Implement base site from Polymer Starter Kit

--- gulp_tasks/_config.js
+++ gulp_tasks/_config.js
@@ -6,7 +6,6 @@ var manifest = require(__dirname + '/../package.json');
 
 var config = {};
 
-config.browsersync = {};
 config.files = {};
 config.path = {};
 config.port = {};
@@ -20,8 +19,8 @@ config.path.nodeModules = path.normalize(__dirname + '/../node_modules');
 config.path.bowerComponents = config.path.root + '/bower_components';
 
 config.path.markup = config.path.base;
-config.path.scripts = config.path.base + '/js';
-config.path.styles = config.path.base + '/sass';
+config.path.scripts = config.path.base + '/scripts';
+config.path.styles = config.path.base + '/styles';
 config.path.styleguide = config.path.base + '/styleguide';
 
 // Files
@@ -30,15 +29,28 @@ config.files.scripts = config.path.scripts + '/**/*.js';
 config.files.styles = config.path.styles + '/**/*.scss';
 
 // BrowserSync
-config.browsersync.files = [
-  config.path.base + '/**',
-  '!' + config.path.base + '/**.map'
-];
-
-config.browsersync.browser = ['google chrome'];
-config.browsersync.open = false
-config.browsersync.port = manifest.config.server.browsersync.port;
-config.browsersync.proxy = 'http://localhost:' + process.env.PORT ? process.env.PORT : manifest.config.server.http.port;
-// config.browsersync.tunnel = true;
+config.browsersync = {
+  browser: ['google chrome'],
+  files: [
+    config.path.base + '/**',
+    '!' + config.path.base + '/**.map'
+  ],
+  notify: false,
+  open: false,
+  port: manifest.config.server.browsersync.port,
+  proxy: 'http://localhost:' + process.env.PORT ? process.env.PORT : manifest.config.server.http.port,
+  server: 'www',
+  snippetOptions: {
+    rule: {
+      match: '<span id="browser-sync-binding"></span>',
+      fn: function(snippet) {
+        return snippet;
+      }
+    }
+  },
+  ui: {
+    port: manifest.config.server.browsersync.ui.port
+  }
+};
 
 module.exports = config;
