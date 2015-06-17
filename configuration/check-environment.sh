8bab5159e4b5aaaaee253380c4c4701b766bc7be
Karl Podger <karl.podger@primeordinal.com>
2015-06-17 01:14:37 +0100

    Fix logic in check-environment shell script

--- configuration/check-environment.sh
+++ configuration/check-environment.sh
@@ -11,58 +11,65 @@ if [ -f .git/config ]; then
 
 fi
 
-if [ -f Gemfile ]; then
-
-  if hash ruby 2>/dev/null; then
-
-    if hash update_rubygems 2>/dev/null; then
-      #sudo update_rubygems
-      update_rubygems
-    else
-      echo "update_rubygems not installed"
-    fi
-
-    if hash gem 2>/dev/null; then
-      gem update --system
-      gem install bundler
-    else
-      echo "gem not installed"
-      exit
-    fi
-
-    if hash bundle 2>/dev/null; then
-      bundle install
-    else
-      echo "bundle not installed"
-      exit
-    fi
-
-  else
-    echo "ruby not installed"
-    exit
-  fi
-
-fi
+# if [ -f Gemfile ]; then
+#
+#   if hash ruby 2>/dev/null; then
+#
+#     if hash update_rubygems 2>/dev/null; then
+#       #sudo update_rubygems
+#       update_rubygems
+#     else
+#       echo "update_rubygems not installed"
+#     fi
+#
+#     if hash gem 2>/dev/null; then
+#       gem update --system
+#       gem install bundler
+#     else
+#       echo "gem not installed"
+#       exit
+#     fi
+#
+#     if hash bundle 2>/dev/null; then
+#       bundle install
+#     else
+#       echo "bundle not installed"
+#       exit
+#     fi
+#
+#   else
+#     echo "ruby not installed"
+#     exit
+#   fi
+#
+# fi
 
 if [ -f package.json ]; then
+  echo "package.json found"
 
   if hash npm 2>/dev/null; then
+    echo "npm installed"
 
-    if [ -f node_modules ]; then
+    if [ -d node_modules ]; then
+      echo "running npm update"
       npm update
     else
+      echo "running npm install"
       npm install
     fi
 
     if [ -f bower.json ]; then
+      echo "bower.json found"
 
-      if hash bower 2>/dev/null; then
+      if [ -f ./node_modules/.bin/bower ]; then
+        echo "bower installed"
 
-        if [ -f www/bower_components ]; then
-          #bower cache clean;
-          bower update;
+        if [ -d bower_components ]; then
+          echo "running bower update"
+          npm run bower update;
         else
-          bower install;
+          echo "running bower install"
+          npm run bower install;
         fi
 
       else
@@ -72,10 +79,22 @@ if [ -f package.json ]; then
 
     fi
 
-    if [ -f gulpfile.js ]; then
+    if [ -f ./node_modules/.bin/npm-check-updates ]; then
+      echo "npm-check-updates installed"
+      echo "running npm-check-updates"
+      npm run npm-check-updates
+    else
+      echo "npm-check-updates not installed"
+      exit
+    fi
 
-      if hash gulp 2>/dev/null; then
-        gulp
+    if [ -f gulpfile.js ]; then
+      echo "gulpfile.json found"
+
+      if [ -f ./node_modules/.bin/gulp ]; then
+        echo "gulp installed"
+        echo "running gulp"
+        npm run gulp
       else
         echo "gulp not installed"
         exit
