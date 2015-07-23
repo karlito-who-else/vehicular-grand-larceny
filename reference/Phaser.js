7fe9da9bed0fa23afc9d425610d642eeb9f01b83
Karl Podger <karl.podger@primeordinal.com>
2015-07-23 01:12:23 +0100

    Update Phaser reference code

--- reference/Phaser.js
+++ reference/Phaser.js
@@ -1,27 +1,33 @@
+this.game.physics.arcade.velocityFromAngle(this.sprite.angle, 3000, this.sprite.body.velocity);
+
+this.sprite.body.rotateLeft(this.attributes.body.turnSpeed);
+this.sprite.body.rotateRight(this.attributes.body.turnSpeed);
+
+this.sprite.body.angularForce -= this.attributes.body.turnSpeed;
+this.sprite.body.angularForce += this.attributes.body.turnSpeed;
+
+this.sprite.body.moveUp(100);
+this.sprite.body.moveDown(100);
+this.sprite.body.moveLeft(100);
+this.sprite.body.moveRight(100);
+
 accelerate() {
-  // console.log('accelerate', 'velocity', this.sprite.body.velocity, 'angle', this.sprite.angle);
   this.game.physics.arcade.velocityFromAngle(this.sprite.angle, 3000, this.sprite.body.velocity);
-  // this.sprite.body.moveUp(100);
 }
 
 decelerate() {
-  // console.log('accelerate', 'velocity', this.sprite.body.velocity, 'angle', this.sprite.angle);
   if (this.sprite.body.velocity > 0) {
     this.sprite.body.velocity -= ((this.sprite.body.velocity -= this.attributes.body.movementDecelerationSpeed) >= 0) ? this.attributes.body.movementDecelerationSpeed : this.sprite.body.velocity;
   }
-  // this.sprite.body.moveDown(100);
 }
 
 turnLeft() {
   this.sprite.body.angularVelocity -= this.attributes.body.turningAccelerationSpeed;
-  // this.sprite.body.moveLeft(100);
 }
 
 turnRight() {
   this.sprite.body.angularVelocity += this.attributes.body.turningAccelerationSpeed;
-  // this.sprite.body.moveRight(100);
 }
-
 
 //
 data: Body
